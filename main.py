bf6c7088a50fbea58c99171c15687f1d1fee631a
ChenMichaeliPri <chenmichaeli.op@gmail.com>
2022-03-20 18:54:40 +0200

    Creating extract and add password methods.

--- main.py
+++ main.py
@@ -12,22 +12,35 @@ current_logged_user = None
 Builder.load_file('design.kv')
 
 # Classes for the different pages of the app
+
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
+    def login(self, id, password):
+        with open("users.json") as file:
+            users = json.load(file)
+        if (id in users) and (users[id]["user_password"] == password):
+            current_logged_user = id
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong id or password !"
+
 class SignUpScreen(Screen):
     # Adding user to json file database and documenting creation time
-    def add_user(self, userPassword):
+    def add_user(self, user_id, user_password):
         with open("users.json") as file:
             users = json.load(file)
 
-        users["User: "+str(len(users)+1)] = {'userPassword': userPassword, 'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        if user_id in users:
+            self.ids.username_taken.text = "Please select a different user id"
+        else:
+            users[user_id] = {'user_id': user_id, 'user_password': user_password, 'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S"), 'passwords': dict()}
 
-        with open("users.json", 'w') as file:
-            json.dump(users, file)
-            # Transfers user to update page on successful regestriation
-            self.manager.current = "sign_up_screen_success"
+            with open("users.json", 'w') as file:
+                json.dump(users, file)
+                # Transfers user to update page on successful regestriation
+                self.manager.current = "sign_up_screen_success"
 
 class SignUpScreenSuccess(Screen):
     def go_to_login(self):
@@ -35,11 +48,32 @@ class SignUpScreenSuccess(Screen):
         self.manager.current = "login_screen"
 
 class LoginScreenSuccess(Screen):
-    def extract_password(self, required_password):
-        pass
+    def log_out(self):
+        current_logged_user = None
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
 
+    # Returns the required password from the database
+    def extract_password(self, required_password):
+        with open("users.json") as file:
+            passwords = json.load(file)
+
+        if required_password in passwords[current_logged_user]["passwords"]:
+            required_password_output = passwords[current_logged_user]["passwords"][required_password]
+        else:
+            required_password_output = "Wrong password name, please try again !"
+
+    # Adds a new password to the database
     def add_password(self, new_password_name, new_password):
-        pass
+        with open("users.json") as file:
+            passwords = json.load(file)
+
+        passwords[current_logged_user]["passwords"][new_password_name] = new_password
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+
+        required_password_output = "New password added successfully"
 
 class RootWidget(ScreenManager):
     pass
