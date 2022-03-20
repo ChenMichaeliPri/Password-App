e89e6fa936f09411e1306c6a4d8e74474f26d5a4
ChenMichaeliPri <chenmichaeli.op@gmail.com>
2022-03-20 15:41:13 +0200

    Adding a json file to use as a database.

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+# Importing the relevent libraries and packages
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
+# Synching with the kivi file
 Builder.load_file('design.kv')
 
+# Classes for the different pages of the app
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, userPassword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[userPassword] = {'userPassword': userPassword, 'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
 
 class RootWidget(ScreenManager):
     pass
 
+# Main
 class MainApp(App):
     def build(self):
         return RootWidget()
